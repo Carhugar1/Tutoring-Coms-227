533d938dce03d1900315be83de271431a7b0841c
Carhugar1 <Carhugar1@live.com>
2015-02-23 21:34:06 -0600

    .gitignore
    fixed the .gitignore more and changed the file naming system to better
    suit the situation.

--- 2-23-2015/src/Yeah/Main.java
+++ 2-23-2015/src/Yeah/Main.java
@@ -3,8 +3,16 @@ package Yeah;
 import java.util.Scanner;
 
 public class Main {
-
 	
+	/*
+	 * We talked about how helper methods work and why they are useful.
+	 * 
+	 * Helper Methods take duplicated functionality out of the main and move it
+	 * to another method that can be called whenever that functionality is needed.
+	 * 
+	 * In this code we took out the asking and retrieving of the number and moved
+	 * it into the helper method. 
+	 */
 	public static void main(String[] args) {
 		
 		// Create the Scanner from the System.in
@@ -15,29 +23,31 @@ public class Main {
 		System.out.print("Enter a Number: "); // prints "Enter a Number:" but doesn't return after (\n)
 		int first = in.nextInt(); // get the next Integer
 		*/
-		int first = helperMethod(in);
+		int first = helperMethod(in); // replaced above with our helper method
 		
 		/* This has been created in the Helper Method
 		// Ask the User for an input then scan for the input.
 		System.out.print("Enter a Number: "); // prints "Enter a Number:" but doesn't return after (\n)
 		int second = in.nextInt(); // get the next Integer
 		*/
-		int second = helperMethod(in);
+		int second = helperMethod(in); // replaced above with our helper method
 		
+		// Multiply the two numbers together and save the result
 		int result = first * second;
 		
+		// give a good output for us to read
 		System.out.println(first + " times " + second + " is " + result);
 		
 	}
 	
-	// Let get rid of the similar stuff.....
+	// Let get rid of the similar stuff with this helper method
 	private static int helperMethod(Scanner scanner) {
 		
 		// Ask the User for an input then scan for the input.
 		System.out.print("Enter a Number: "); // prints "Enter a Number:" but doesn't return after (\n)
 		int first = scanner.nextInt(); // get the next Integer
 		
-		return first;
+		return first; // return that Integer
 	}
 	
 	
