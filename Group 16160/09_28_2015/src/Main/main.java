4d7299d7911594e86d9d4a21f40155c4c4804768
Carhugar1 <Carhugar1@live.com>
2015-09-28 19:32:50 -0500

    09_28_2015
    We went over the practice exam.

---
+++ Group 16160/09_28_2015/src/Main/main.java
@@ -0,0 +1,121 @@
+package Main;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		/* Problem 1 */
+		
+		System.out.println("Problem 1");
+		
+		System.out.println(foo(2, 1)); // same as if we had an object x and did x.foo();
+		
+		System.out.println(foo(0, -1));
+		
+		System.out.println(foo(1, 1));
+		
+		/* end Problem 1*/
+		
+		
+		
+		
+		/* Problem 2 */
+		
+		System.out.println("\nProblem 2");
+		
+			double x = 2.4;
+			int j = 13;
+			String w = "food";
+			boolean goofy = false;
+		
+		/*
+		 * 	Expression				Value				Type
+		 * 	x > 0					true				boolean
+		 * 	j / 3					4					int
+		 * 	j % 3					1					int
+		 * 	j + 1 = j				14					int
+		 * 	j / 10.0				1.3					double
+		 * 	w + w					"foodfood"			String
+		 * 	j + 1 != j				true				boolean
+		 * 	j + ""					"13"				String
+		 * 	j.length()				error				error
+		 * 	w.length()				4					int
+		 */		
+		
+		System.out.println(x > 0);
+		
+		System.out.println(j / 3);
+		
+		System.out.println(j % 3);
+		
+		// System.out.println(j + 1 = j); // errors in the printing
+		
+		System.out.println(j / 10.0);
+		
+		System.out.println(w + w);
+		
+		System.out.println(j + 1 != j);
+		
+		System.out.println(j + "");
+		
+		// System.out.println(j.length()); // errors normally
+		
+		System.out.println(w.length());
+		
+		/* end Problem 2 */
+		
+		
+		
+		/* Problem 3 */
+		
+		// (a) boolean isOdd = (x %2 != 0);
+		
+		// (b) int fullCartons = eggCount / 12;
+		
+		// (c) int leftOver = eggCount % 12;
+		
+		// (d) char lastChar = str.charAt( str.length() - 1 )
+		
+		// (e) boolean areTheSameString = s1.equals(s2);
+		
+		// (f) boolean hasAtLeastFour = s.length() > 3;
+		
+		// (g) double totalValue = 0.0 + d + ( c / 100.0 )
+		
+		// (h) String s = "" + i;
+		
+		// (i) int i = d - ( d % 1 );
+		
+		/* end Problem 3 */
+		
+		
+		
+
+	}
+	
+	// Helper method given in Problem 1
+	public static boolean foo(int x, int y) {
+		
+		boolean result = false;
+		
+		if (x > y) {
+			
+			if (y != 0) {
+	
+				result = true;
+	
+			}
+	 
+		}
+	 
+		if (x == 0) {
+	 
+			result = true;
+	 
+		}
+	 
+		return result;
+	 
+	}
+
+}
